72f64a5aceca7cf0cd208207d29d4804172c124f
GoldenStone929 <yshawncui@gmail.com>
2023-08-07 17:33:56 -0700

    test Job_pane Implemention, on the right path, need more testing and detail fixing

--- com/scitegic/proxy/examples/Tree_Infor.java
+++ com/scitegic/proxy/examples/Tree_Infor.java
@@ -1,34 +1,34 @@
 package com.scitegic.proxy.examples;
 
-
-import com.scitegic.proxy.ComponentDatabase;
-import com.scitegic.proxy.PipelinePilotServer;
-import com.scitegic.proxy.XmldbItem;
-import com.scitegic.proxy.PipelinePilotServerConfig;
-import com.scitegic.proxy.Job;
-
-
 import com.scitegic.proxy.*;
 
 import javax.swing.*;
 import javax.swing.tree.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.File;
 
 public class Tree_Infor {
 
     static final String WEB_PORT_EXAMPLE_PROTOCOLS = "Protocols/Web Services/Web Port Examples";
-    static final String PROTOCOL = WEB_PORT_EXAMPLE_PROTOCOLS + "/Generic/XY Scatter Plot Utility";
 
-    public Tree_Infor() {}
+    public Tree_Infor() {
+    }
 
-    private static DefaultMutableTreeNode buildTreeRecursive(XmldbItem folder) {
+    private static DefaultMutableTreeNode buildTreeRecursive(XmldbItem folder, int indent) {
+        if (folder.getName().toLowerCase().equals("utilities")) {
+            return null;
+        }
         DefaultMutableTreeNode currentFolder = new DefaultMutableTreeNode(folder.getName());
+        indent += 2;
         XmldbItem[] children = folder.getChildren();
-        for (XmldbItem child : children) {
+        for (int i = 0, m = children.length; i < m; i++) {
+            XmldbItem child = children[i];
             if (child.isFolder()) {
-                currentFolder.add(buildTreeRecursive(child));
+                DefaultMutableTreeNode childFolder = buildTreeRecursive(child, indent);
+                if (childFolder != null) {
+                    currentFolder.add(childFolder);
+                }
             } else {
                 currentFolder.add(new DefaultMutableTreeNode(child.getName()));
             }
@@ -46,78 +46,40 @@ public class Tree_Infor {
         String password = args[2];
 
         PipelinePilotServer pp = null;
-        Job protocol = null;
 
         try {
             pp = new PipelinePilotServer(server, user, password);
-            PipelinePilotServerConfig conf = pp.getServerConfig();
-            ComponentDatabase compdb = pp.getComponentDatabase();
 
-            XmldbItem rootFolder = compdb.getXmldbContentsRecursive(WEB_PORT_EXAMPLE_PROTOCOLS);
-            DefaultMutableTreeNode treeRoot = buildTreeRecursive(rootFolder);
+            XmldbItem rootFolder = pp.getComponentDatabase().getXmldbContentsRecursive(WEB_PORT_EXAMPLE_PROTOCOLS);
+            DefaultMutableTreeNode treeRoot = buildTreeRecursive(rootFolder, 0);
 
-            JTree tree = new JTree(treeRoot);
-            tree.addMouseListener(new MouseAdapter() {
-                public void mouseClicked(MouseEvent me) {
-                    TreePath tp = tree.getPathForLocation(me.getX(), me.getY());
-                    if (tp != null) {
-                        DefaultMutableTreeNode node = (DefaultMutableTreeNode) tp.getLastPathComponent();
-                        if (node.isLeaf()) {
-                            new JobPane(node.getUserObject().toString());
+            SwingUtilities.invokeLater(() -> {
+                JFrame frame = new JFrame("File Structure");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new BorderLayout());
+
+                JTree tree = new JTree(treeRoot);
+                JScrollPane scrollPane = new JScrollPane(tree);
+                frame.add(scrollPane, BorderLayout.CENTER);
+
+                tree.addMouseListener(new MouseAdapter() {
+                    public void mouseClicked(MouseEvent me) {
+                        TreePath tp = tree.getPathForLocation(me.getX(), me.getY());
+                        if (tp != null && tp.getLastPathComponent() != null) {
+                            String filename = tp.getLastPathComponent().toString();
+                            // When a file is clicked in the tree, show the corresponding job pane
+                            JobPane.showJobPane(filename);
                         }
                     }
-                }
+                });
+
+                frame.setSize(500, 500);
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
             });
 
-            JFrame frame = new JFrame("File Structure");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.add(new JScrollPane(tree));
-            frame.setSize(500, 500);
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
-
-            protocol = pp.createJob(PROTOCOL);
-            boolean uploadFromClient = true;
-            if (uploadFromClient) {
-                File localFile = new File("./data/imports-85.txt");
-                protocol.setInputFileOnClient("Source", localFile);
-            } else {
-                protocol.setInputValue("Source", "data/Tables/imports-85.txt");
-            }
-
-            protocol.setInputValue("X Property", "Highwaympg");
-            protocol.setInputValue("Y Property", "Horsepower");
-            protocol.setInputValue("Tooltip",
-                    "'Make = ' . (make) . ', $(X Property) = ' . ($(X Property)) . ', "
-                            + "$(Y Property) = ' . ($(Y Property))");
-            protocol.setInputValue("File Type", "PDF");
-            protocol.validate();
-
-            protocol.run();
-
-            JobStatus status = protocol.getStatus();
-            while (!status.isExitStatus()) {
-                Thread.sleep(2000);
-                status = protocol.getStatus();
-            }
-
-            if (JobStatus.Finished.equals(status)) {
-                String[] results = protocol.getJobResults().getResultFiles();
-
-                if (results.length > 0) {
-                    File localResultFile = new File("chart.pdf");
-                    pp.getRemoteFileManager().downloadFile(results[0], localResultFile);
-                }
-            }
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (protocol != null) {
-                try {
-                    protocol.releaseJob();
-                } catch (Exception ex) {
-                }
-            }
         }
     }
 }
